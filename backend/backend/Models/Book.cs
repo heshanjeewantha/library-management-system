432f736843df269cae1516e050fa5055b5d5b879
it22578532 <heshanjeewantha@gmail.com>
2025-06-20 19:46:31 +0530

    add backend validation for book.cs

--- backend/backend/Models/Book.cs
+++ backend/backend/Models/Book.cs
@@ -1,10 +1,20 @@
-﻿namespace backend.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models
 {
     public class Book
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(200, MinimumLength = 1)]
         public string Title { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 1)]
         public string Author { get; set; }
+
+        [StringLength(1000)]
         public string Description { get; set; }
     }
 }
